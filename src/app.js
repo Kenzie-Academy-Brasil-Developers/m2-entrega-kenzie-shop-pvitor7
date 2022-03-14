81c71f0cfc68c40bd281ad7c38e6a967ad01c98d
Paulo Vitor <pvitor7@hotmail.com>
2022-03-13 23:07:16 -0300

    Falta alinhar os elementos CSS

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ class app {
 
     static async requisicao() {
 
-       await fetch('https://m2-kenzie-shop.herokuapp.com/products')
+        await fetch('https://m2-kenzie-shop.herokuapp.com/products')
             .then(response => response.json())
             .then((data) => {
 
@@ -17,27 +17,62 @@ class app {
     static template(object) {
 
         const div = document.createElement('div');
-        div.id = 'produto'
-        
         const img = document.createElement('img');
+        const totalEstrelas = document.createElement('div')
         const p = document.createElement('p');
         const price = document.createElement('p');
         const buttonShop = document.createElement('button');
 
+        div.id = 'produto'
         img.id = 'imgProduct';
+        totalEstrelas.id = 'totalEstrelas'
         p.id = 'productInfo';
         price.id = "preco";
         buttonShop.id = 'botaoComprar';
-        img.style = `background-image: url(https://kenzie-academy-brasil.gitlab.io/fullstack/frontend/modulo2/sprint3/img/consumindo-api-produtos/${object.id}/Image.png)`;
+        img.src = `https://kenzie-academy-brasil.gitlab.io/fullstack/frontend/modulo2/sprint3/img/consumindo-api-produtos/${object.id}/Image.png`;
         p.innerHTML = object.productName;
-        price.innerHTML = `R$ ${object.price.productPrice},00`;
         buttonShop.innerHTML = `Comprar`;
 
         div.appendChild(img);
-        div.appendChild(p);
-        div.appendChild(price);
-        div.appendChild(buttonShop);
+        div.appendChild(totalEstrelas);
 
+        const estrelasCheias = object.reviews;
+        const estrelasVazias = 5 - object.reviews;
+
+        for (let i = 0; i < estrelasCheias; i++) {
+            const estrela = document.createElement('div')
+            estrela.id = 'estrela-cheia';
+            totalEstrelas.appendChild(estrela);
+        }
+
+        for (let i = 0; i < estrelasVazias; i++) {
+            const estrela = document.createElement('div')
+            estrela.id = 'estrela-vazia';
+            totalEstrelas.appendChild(estrela);
+        }
+
+        div.appendChild(p);
+
+        if (object.promotionStatus === true) {
+
+            const precoOriginal = document.createElement('p');
+            precoOriginal.id = 'precoOriginal'
+            precoOriginal.innerHTML = `De: R$ ${object.price.productPrice},00`;
+
+            const preco = object.price.productPromotionPrice.toFixed(2).replace(/[.]/g, ',');
+            console.log(preco);
+            price.innerHTML = `Por: R$${preco}`
+
+            div.appendChild(precoOriginal);
+            div.appendChild(price);
+
+        } else {
+
+            price.innerHTML = `R$ ${object.price.productPrice},00`;
+            div.appendChild(price);
+        }
+
+        div.appendChild(buttonShop);
         areaProdutos.appendChild(div);
 
         return div;
@@ -45,5 +80,12 @@ class app {
 
 }
 
+
+
+
 app.requisicao()
 
+function estrelas(object) {
+
+
+}
